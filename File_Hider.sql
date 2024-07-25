189757fb4df3bfc482a52435b945253733490ad7
Omkar Rajesh Auti <omkarauti11052001@gmail.com>
2024-07-25 22:25:53 +0530

    SQL file added.

---
+++ File_Hider.sql
@@ -0,0 +1,12 @@
+create database File_Hider;
+
+use File_Hider;
+
+create table users(id int primary key auto_increment, name varchar(100),email varchar(100) unique);
+desc users;
+
+create table data(id int primary key auto_increment, name varchar(100),path varchar(255),email varchar(100), bin_data blob);
+desc data;
+
+select * from users;
+select * from data;
