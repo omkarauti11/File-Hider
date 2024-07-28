c9214a216e5fc04ff0c64ef3b184e8055dd8150b
Omkar Auti <122270586+omkarauti11@users.noreply.github.com>
2024-07-28 19:58:57 +0530

    Update File_Hider.sql
    Change Data Type of bin_data from BLOB to LONGBLOB to store file which is of size upto 4 GB

--- File_Hider.sql
+++ File_Hider.sql
@@ -5,7 +5,7 @@ use File_Hider;
 create table users(id int primary key auto_increment, name varchar(100),email varchar(100) unique);
 desc users;
 
-create table data(id int primary key auto_increment, name varchar(100),path varchar(255),email varchar(100), bin_data blob);
+create table data(id int primary key auto_increment, name varchar(100),path varchar(255),email varchar(100), bin_data LONGBLOB);
 desc data;
 
 select * from users;
